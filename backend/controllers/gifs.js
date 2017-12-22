de349b56066495e8764641c5a136fb727ff0a0c7
Rashad Hammonds <rhammonds@yearup.org>
2017-12-22 14:11:08 -0500

    first commit

--- backend/controllers/gifs.js
+++ backend/controllers/gifs.js
@@ -9,12 +9,18 @@ const Gif = mongoose.model('gif')
 // sets the model again.
 
 // exports to router
-
 Router.get('/', (req, res) => {
   Gif
     .find({})
     .then(gifs => res.json(gifs))
 })
+// exports all of seed data too root location.
+Router.get('/:title', (req, res) => {
+  Gif
+      .findOne({title: req.params.title})
+      .then(gifs => res.json(gifs))
+})
+// finds databse of gif, creates new and reutnrs all of it
 
 Router.post('/', (req, res) => {
   Gif
@@ -25,4 +31,16 @@ Router.post('/', (req, res) => {
         .then(gifs => res.json(gifs))
       })
 })
+// finds databse of gif, creates new and reutnrs all of it
+Router.put('/:title', (req, res) => {
+  Gif
+    .findOneAndUpdate({title: req.params.title}, req.body)
+    .then(gifs => res.json(gifs))
+})
+Router.delete('/:title', (req, res) => {
+  Gif
+      .findOneAndRemove({ title: req.params.title })
+      .then(gifs => res.json(gifs))
+})
+//  allows you to update the files
 module.exports = Router
